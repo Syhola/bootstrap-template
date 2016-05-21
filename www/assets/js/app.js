e2f9c0369fe091ac70d47a7e9e3549e03e0bc88a
Syho <syho.webmaster@gmail.com>
2016-05-21 12:15:57 +0200

    project organisation

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -1,6 +1,5 @@
 var app = angular.module('bootstraptemplate', [
   'ngRoute',
-  'controllers',
   'firebase'
 ]);
 
@@ -19,6 +18,52 @@ app.config(['$routeProvider',
 
 app.controller('firstCtrl', ['$scope', '$location', '$route', '$firebaseAuth', '$firebaseObject', '$firebaseArray',
   function ($scope, $location, $route, $firebaseAuth, $firebaseObject, $firebaseArray){
-  }]);
 
-var bootstraptemplateControllers = angular.module('controllers', []);
+    console.log("Hello World!");
+    
+    var ref = new Firebase('https://bootstrap-template.firebaseio.com/');
+
+    // Check authification
+      var authData = ref.getAuth();
+      $scope.authObj = $firebaseAuth(ref);
+
+      if (authData) {
+        console.log("User " + authData.uid + " is logged in with " + authData.provider);
+        $scope.log = true;
+        $scope.youremail = authData.password.email;
+        var uid = authData.uid;
+      } else {
+        console.log("User is logged out");
+      }
+
+    // Fonction create user
+        $scope.register = function() {
+
+          $scope.authObj.$createUser({
+            email: $scope.inputEmail,
+            password: $scope.inputPassword
+          }).then(function(userData) {
+            console.log("User " + userData.uid + " created successfully!");
+
+            return $scope.authObj.$authWithPassword({
+              email: $scope.email,
+              password: $scope.password
+            });
+          }).then(function(authData) {
+            console.log("Logged in as:", authData.uid);
+            $location.path('/stats');
+            isAuth = true;
+          }).catch(function(error) {
+            console.error("Error: ", error);
+          });
+
+      }
+
+    // Fonction logout
+    $scope.logout = function() {
+
+        ref.unauth();
+        $route.reload();
+
+      }
+  }]);
--- www/old/controller.js
+++ www/old/controller.js
@@ -1,5 +1,6 @@
-//controllers.controller('firstCtrl', ['$scope', '$location', '$route', '$firebaseAuth', '$firebaseObject', '$firebaseArray'
 firstCtrl = function ($scope, $location, $route, $firebaseAuth, $firebaseObject, $firebaseArray) {
+
+  console.log("Hello World!");
 
   var ref = new Firebase('https://bootstrap-template.firebaseio.com/');
 
@@ -17,11 +18,11 @@ firstCtrl = function ($scope, $location, $route, $firebaseAuth, $firebaseObject,
     }
 
   // Fonction create user
-      $scope.register = function(){
+      $scope.register = function() {
 
         $scope.authObj.$createUser({
-          email: $scope.email,
-          password: $scope.password
+          email: $scope.inputEmail,
+          password: $scope.inputPassword
         }).then(function(userData) {
           console.log("User " + userData.uid + " created successfully!");
 
@@ -40,7 +41,7 @@ firstCtrl = function ($scope, $location, $route, $firebaseAuth, $firebaseObject,
     }
 
   // Fonction logout
-  $scope.logout = function(){
+  $scope.logout = function() {
 
       ref.unauth();
       $route.reload();
@@ -48,4 +49,3 @@ firstCtrl = function ($scope, $location, $route, $firebaseAuth, $firebaseObject,
     }
 
 }
-//]);
